86ed756fa2f50bfe3881422c5d1207c6c294b49a
dkabir94@bu.edu <Dkabir94@bu.edu>
2021-05-04 19:12:11 -0500

    adding jquery

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
 
 
-var jqxhr = $.get( "http://zumatra.com/teach/colors.php", function() {
-    alert( "success" );
-  })
-    .done(function() {
-      alert( "second success" );
-    })
-    .fail(function() {
-      alert( "error" );
-    })
-    .always(function() {
-      alert( "finished" );
-    });
